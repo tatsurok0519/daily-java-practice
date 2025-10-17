d247fabb168e8110b5b6d002960112f11ccd0fb5
tatsurok0519 <arrivederci0519@yahoo.co.jp>
2025-10-17 10:05:07 +0900

    MyApp28

---
+++ MyApp28.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class MyApp28 {
+  public static void main(String[] args) {
+    // for (int i = 0; i < 10; i++) {
+    for (int i = 1; i <= 10; i++) {
+      System.out.println(i + " Hello");
+    }
+  }
+}
