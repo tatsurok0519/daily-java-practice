7c723b02096c92f29ff69c740dba9478f3c6c12c
tatsurok0519 <arrivederci0519@yahoo.co.jp>
2025-10-17 09:17:25 +0900

    MyApp15

---
+++ MyApp15.java
@@ -0,0 +1,10 @@
+public class MyApp15 {
+  public static void main(String[] args) {
+    int[] scores = {70, 90, 80};
+    // int[] scoresBackup = scores;
+    int[] scoresBackup = {70, 90, 80};
+    scores[1] = 100;
+    System.out.println(scores[1]);
+    System.out.println(scoresBackup[1]);
+  }
+}
